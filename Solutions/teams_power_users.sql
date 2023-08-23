ae2e2f3bdc8448451f76f996177d0f8623d16c6b
Dennis <domoreburpees@icloud.com>
2023-08-23 13:04:32 -0600

    practice

---
+++ Solutions/teams_power_users.sql
@@ -0,0 +1,5 @@
+SELECT sender_id, COUNT(sender_id) AS message_count FROM messages
+WHERE DATE_PART('month', sent_date::date)=08 AND DATE_PART('year', sent_date::date)=2022
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2;
