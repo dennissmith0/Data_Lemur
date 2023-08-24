a6e777f5f63309ed86dd5c5f258212f576db56d1
Dennis <domoreburpees@icloud.com>
2023-08-24 14:09:18 -0600

    practice

--- Solutions/highest_grossing_items.sql
+++
@@ -1,5 +0,0 @@
-SELECT category, product, SUM(spend) AS total_spend FROM product_spend
-WHERE EXTRACT(YEAR FROM transaction_date) = '2022'
-GROUP BY category, product
-ORDER BY total_spend DESC
-LIMIT 2;
