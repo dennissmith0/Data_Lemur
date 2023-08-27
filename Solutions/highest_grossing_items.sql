a1ed7b2ed8094bc59624d006510e8cdc67c8b147
Dennis <domoreburpees@icloud.com>
2023-08-27 15:26:04 -0600

    Update highest_grossing_items.sql

---
+++ Solutions/highest_grossing_items.sql
@@ -0,0 +1,11 @@
+WITH Ranked_Products AS (
+    SELECT
+    category,
+    product,
+    SUM(spend) as total_spend,
+    ROW_NUMBER() OVER(PARTITION BY category ORDER BY SUM(spend) DESC) AS rnk
+    WHERE EXTRACT(YEAR FROM transaction_date)='2022'
+    GROUP BY category, product
+)
+SELECT category, product, total_spend FROM Ranked_Products
+WHERE rnk <= 2;
